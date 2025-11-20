20c5360dac140bc04c2fffc575b9a5a75fdc16d9
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2025-11-20 17:35:58 +0100

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 5555;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.jscon());
+
+
+app.get("/", async (req, res)=> {
+
+res.render("accueil");
+
+
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`ok sur le port ${PORT}`);
+})
