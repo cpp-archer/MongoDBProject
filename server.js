2315ef778fabe80bdadd851c11497712f61f0c0d
raphaelrif <raphael63link@gmail.com>
2025-11-25 14:47:28 +0100

    pour jenna

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5555;
 
+mongoose.connect("mongodb://localhost:27017/bibliotheque");
 // app.use(express.urlencoded({extended:true}));
 // app.use(express.jscon());
 
@@ -9,11 +11,18 @@ const PORT = 5555;
 app.get("/", async (req, res)=> {
 
 res.redirect("/accueil");
-
-
 });
 
 
 app.listen(PORT, ()=>{
     console.log(`ok sur le port ${PORT}`);
 })
+
+const Film = mongoose.model("films",{
+    titre:String,
+    annee:Number,
+    genre:String
+});
+Film.find().then(console.log);
+
+
