4977391d19e17b1b31036d441f665e1746c5d6d8
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2025-11-25 14:32:39 +0100

    accueil

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 5555;
-
+const path = require("path");
 // app.use(express.urlencoded({extended:true}));
 // app.use(express.jscon());
 
 
-app.get("/", async (req, res)=> {
+// app.get("/", async (req, res)=> {
 
-res.redirect("/accueil");
+// res.redirect("/accueil");
 
 
+// });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/accueil.html"));
 });
 
 
