416e6d7a9b1d7e3d7db87775df2de76d82e07847
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2025-11-25 15:42:14 +0100

    okak

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5555;
-const path = require("path");
+
+mongoose.connect("mongodb://localhost:27017/BDDrj");
 // app.use(express.urlencoded({extended:true}));
 // app.use(express.jscon());
 
 
 // app.get("/", async (req, res)=> {
 
-// res.redirect("/accueil");
+// // res.redirect("/accueil");
+// // });
 
-
-// });
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/accueil.html"));
-});
 
 
 app.listen(PORT, ()=>{
     console.log(`ok sur le port ${PORT}`);
 })
+
+const Film = mongoose.model("taches",{
+    titre:String,
+    description:String,
+    dateCreation:Date,
+    echeance:Date,
+    statut:String,
+    priorite:String,
+    auteur: { String},
+    categorie: String,
+    etiquettes: [ String],
+    'sous-taches': [Object],
+    commentaires: [ Object ],
+    historiqueModifications: [ Object ]
+});
+Film.find().then(console.log);
+
+
