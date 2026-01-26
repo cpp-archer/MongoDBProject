e455438e0b24a23c24198b478a646600e37da5d0
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-01-26 11:15:49 +0100

    redef

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+
+const Task = require ("./models/Task");
+
 const app = express();
 const PORT = 5555;
 
 mongoose.connect("mongodb://localhost:27017/BDDrj");
-// app.use(express.urlencoded({extended:true}));
-// app.use(express.jscon());
 
 
-// app.get("/", async (req, res)=> {
-
-// // res.redirect("/accueil");
-// // });
-
-const path = require("path");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public"))); //pour html css js 
 
 
+//accueil
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/accueil.html"));
+    res.sendFile(path.join(__dirname,"public", "/accueil.html"));
 });
 
+//liste des taches
+app.get("/TasksList", (req, res) => {
+    res.sendFile(path.join(__dirname,"public", "/TasksList.html"));
+});
 
+//page pour creer une tache
+app.get("/createTask", (req, res) => {
+    res.sendFile(path.join(__dirname,"public", "/createTask.html"));
+});
+
+//info d'une tache
+app.get("/taskInfo/:id", (req, res) => {
+    res.sendFile(path.join(__dirname,"public", "/taskInfo.html"));
+});
 
 app.listen(PORT, ()=>{
     console.log(`ok sur le port ${PORT}`);
