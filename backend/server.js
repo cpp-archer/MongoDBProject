35db00dfda615899f925ca7cc5ef6669576a9b20
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-01-29 00:54:39 +0100

    CA MARCHE avec un seul serv back

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const path = require("path");
-
-
 const tasksRoutes = require("./routes/tasksRoute");
 
 const app = express();
@@ -15,21 +13,34 @@ mongoose.connect("mongodb://localhost:27017/BDDrj");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname, "front"))); //pour html css js
-
-//accueil
-app.get("/", (req, res) => {
-    res.json({ message: "api good " });
-});
+app.use(express.static(path.join(__dirname, "../frontend/public"))); //pour html css js
 
 //on recup les routes API
-app.use("/api/tasks", tasksRoutes); 
+app.use("/apiTasks", tasksRoutes); 
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3001",
-  credentials: true
-}));
+app.get("/api", (req, res) => {    
+    res.json({ message: "api good " });
+
+});
+
+
+//routes html 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/public/html", "accueil.html"));
+});
+
+app.get("/TasksList", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/public/html", "TasksList.html"));
+});
+
+app.get("/createTask", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/public/html", "createTask.html"));
+});
+
+app.get("/taskInfo/:id", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/public/html", "taskInfo.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`ça marche sur le port ${PORT}`);
+    console.log(`ça marche sur http://localhost:${PORT}`);
 });
